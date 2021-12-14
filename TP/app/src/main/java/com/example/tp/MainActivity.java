e22366a152e18fb6d4d73c4875af9417edec3c44
AngeliqueLy <angelique.ly404@gmail.com>
2021-12-14 14:16:46 +0100

    TP
    TP

---
+++ TP/app/src/main/java/com/example/tp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.tp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MoviesService moviesService = new Retrofit.Builder()
+                .baseUrl( MoviesService.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create( MoviesService.class);
+
+       moviesService.getPopularMovies().enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                afficherMovie(response.body().getitem());
+            }
+
+            @Override
+
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+              t.getCause();
+            }
+
+        });
+    }
+    public void afficherMovie(List<Movie> movie) {
+
+        Toast.makeText(this,"nombre de film : "+movie.size(), Toast.LENGTH_SHORT).show();
+
+    }
+
+
+        }
+
+
